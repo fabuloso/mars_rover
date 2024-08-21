717fb5bde7ad9e43d2445b598b3e7e5d8eb89c38
Alessandro <dalfovo.alessandro@gmail.com>
2024-08-21 14:25:53 +0200

    First step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
     println!("Welcome to Mars");
 }
+
+#[derive(Default)]
+struct Rover {
+    position: (u32, u32),
+    direction: Direction,
+}
+
+impl Rover {
+    fn position(&self) -> (u32, u32) {
+        self.position
+    }
+
+    fn direction(&self) -> Direction {
+        self.direction
+    }
+}
+
+#[derive(Copy, Clone, Default, PartialEq, Eq, Debug)]
+enum Direction {
+    #[default]
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn instantiate_the_rover() {
+        let rover = Rover::default();
+
+        assert_eq!(rover.position(), (0, 0));
+        assert_eq!(rover.direction(), Direction::NORTH);
+    }
+}
